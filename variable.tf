0c10346530fbc75ca4a77518e47e688504a837a4
pradeep <125849033+Pradeepkv93@users.noreply.github.com>
2023-03-19 00:50:52 +0530

    .tfvars

--- variable.tf
+++ variable.tf
@@ -1,24 +1,19 @@
 variable "root_ec2_ami" {
     type = string
-    default = "ami-067871d950411e643"
 }
 
 variable "root_ec2_type" {
     type = string
-    default = "t2.micro"
 }
 
 variable "root_sg_name" {
     type = string
-    default = "my_sg"
 }
 
 variable "root_key_name" {
     type = string
-    default = "my_pem"
 }
 
 variable "root_key_path" {
     type = string
-    default = "./keys/my_pem.pem"
 }
