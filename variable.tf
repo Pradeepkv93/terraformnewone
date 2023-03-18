6537263d6b88f901a532e6d20bfb0c02057d1f6c
pradeep <125849033+Pradeepkv93@users.noreply.github.com>
2023-03-18 23:48:31 +0530

    module

--- variable.tf
+++ variable.tf
@@ -1,25 +1,24 @@
-
-variable "ec2_type" {
-    type = string
-    default = "t2.micro"
-}
-
-variable "ami_id" {
+variable "root_ec2_ami" {
     type = string
     default = "ami-067871d950411e643"
 }
 
-variable "key_name" {
+variable "root_ec2_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "root_sg_name" {
+    type = string
+    default = "my_sg"
+}
+
+variable "root_key_name" {
     type = string
     default = "my_pem"
 }
 
-variable "key_path" {
+variable "root_key_path" {
     type = string
     default = "./keys/my_pem.pem"
-}
-
-variable "sg_name" {
-    type = string
-    default = "my_sg"
 }
